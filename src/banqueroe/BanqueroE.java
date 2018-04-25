014f2e6466a977ea6d0662af77b1505669032540
Juan Ayala Lopez <Juan_Atzuya@hotmail.com>
2018-04-24 21:51:55 -0500

    Impresion de variables

--- src/banqueroe/BanqueroE.java
+++ src/banqueroe/BanqueroE.java
@@ -7,6 +7,8 @@ package banqueroe;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -14,17 +16,32 @@ import java.util.List;
  */
 public class BanqueroE {
 
-    /**
-     * @param args the command line arguments
-     */
+    static Random random = new Random();
+    static List<Proceso> asignados = new ArrayList<Proceso>();
+    static List<Proceso> necesarios = new ArrayList<Proceso>();
+    static Disponibles disponibles = new Disponibles();
+
     public static void main(String[] args) {
         // TODO code application logic here
         String cadenaAsignados = "";
         String cadenaMaximos = "";
         String cadena = "";
-        List<Proceso> asignados = creaAsignados();
-        List<Proceso> necesarios = creaNecesarios();
-        Disponibles disponibles = creaDisponibles();
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduzca el numero de recursos: ");
+        int numeroRecursos = teclado.nextInt();
+        for (int i = 0; i < numeroRecursos; i++) {
+            disponibles.getDisponibles().add(0);
+        }
+        System.out.println("Introduzca el numero de Clientes: ");
+        int numeroProcesos = teclado.nextInt();
+        for (int i = 0; i < numeroProcesos; i++) {
+            necesarios.add(new Proceso());
+            asignados.add(new Proceso());
+        }
+        creaAsignados();
+        creaNecesarios();
+        creaDisponibles();
+        //disponibles = actualizarDisponibles(disponibles);
         for (int i = 0; i < asignados.size(); i++) {
             cadenaAsignados = "|";
             cadenaMaximos = "|";
@@ -42,34 +59,51 @@ public class BanqueroE {
         Comprobaciones.devuelveLista();
     }
 
-    public static List<Proceso> creaAsignados() {
-        List<Proceso> recursosAsignados = new ArrayList<Proceso>();
-        recursosAsignados.add(new Proceso());
-        recursosAsignados.add(new Proceso());
-        recursosAsignados.add(new Proceso());
-        recursosAsignados.add(new Proceso());
-        recursosAsignados.get(0).getRecursosNecesarios().add(1);
-        recursosAsignados.get(0).getRecursosNecesarios().add(0);
-        recursosAsignados.get(0).getRecursosNecesarios().add(0);
-        recursosAsignados.get(1).getRecursosNecesarios().add(6);
-        recursosAsignados.get(1).getRecursosNecesarios().add(1);
-        recursosAsignados.get(1).getRecursosNecesarios().add(2);
-        recursosAsignados.get(2).getRecursosNecesarios().add(2);
-        recursosAsignados.get(2).getRecursosNecesarios().add(1);
-        recursosAsignados.get(2).getRecursosNecesarios().add(1);
-        recursosAsignados.get(3).getRecursosNecesarios().add(0);
-        recursosAsignados.get(3).getRecursosNecesarios().add(0);
-        recursosAsignados.get(3).getRecursosNecesarios().add(2);
-        return recursosAsignados;
+    public static void creaAsignados() {
+        //recursosAsignados.add(new Proceso());
+        //recursosAsignados.add(new Proceso());
+        //recursosAsignados.add(new Proceso());
+        //recursosAsignados.add(new Proceso());
+        //recursosAsignados.get(0).getRecursosNecesarios().add(1);
+        //recursosAsignados.get(0).getRecursosNecesarios().add(0);
+        //recursosAsignados.get(0).getRecursosNecesarios().add(0);
+        //recursosAsignados.get(1).getRecursosNecesarios().add(6);
+        //recursosAsignados.get(1).getRecursosNecesarios().add(1);
+        //recursosAsignados.get(1).getRecursosNecesarios().add(2);
+        //recursosAsignados.get(2).getRecursosNecesarios().add(2);
+        //recursosAsignados.get(2).getRecursosNecesarios().add(1);
+        //recursosAsignados.get(2).getRecursosNecesarios().add(1);
+        //recursosAsignados.get(3).getRecursosNecesarios().add(0);
+        //recursosAsignados.get(3).getRecursosNecesarios().add(0);
+        //recursosAsignados.get(3).getRecursosNecesarios().add(2);
+        Scanner teclado = new Scanner(System.in);
+        int numero = 0;
+        System.out.println("Introduzca los valores para la matriz de Asignados: ");
+        for (int i = 0; i < asignados.size(); i++) {
+            for (int j = 0; j < disponibles.getDisponibles().size(); j++) {
+                System.out.print("Posicion[" + i + "][" + j + "]: ");
+                numero = teclado.nextInt();
+                asignados.get(i).getRecursosNecesarios().add(numero);
+            }
+        }
     }
 
-    public static List<Proceso> creaNecesarios() {
-        List<Proceso> recursosMaximos = new ArrayList<Proceso>();
-        recursosMaximos.add(new Proceso());
-        recursosMaximos.add(new Proceso());
-        recursosMaximos.add(new Proceso());
-        recursosMaximos.add(new Proceso());
-        recursosMaximos.get(0).getRecursosNecesarios().add(3);
+    public static void creaNecesarios() {
+        Scanner teclado = new Scanner(System.in);
+        int numero = 0;
+        System.out.println("Introduzca los valores para la matriz de Necesarios: ");
+        for (int i = 0; i < necesarios.size(); i++) {
+            for (int j = 0; j < disponibles.getDisponibles().size(); j++) {
+                System.out.print("Posicion[" + i + "][" + j + "]: ");
+                numero = teclado.nextInt();
+                necesarios.get(i).getRecursosNecesarios().add(numero);
+            }
+        }
+        //recursosMaximos.add(new Proceso());
+        //recursosMaximos.add(new Proceso());
+        //recursosMaximos.add(new Proceso());
+        //recursosMaximos.add(new Proceso());
+        /*recursosMaximos.get(0).getRecursosNecesarios().add(3);
         recursosMaximos.get(0).getRecursosNecesarios().add(2);
         recursosMaximos.get(0).getRecursosNecesarios().add(2);
         recursosMaximos.get(1).getRecursosNecesarios().add(6);
@@ -80,16 +114,33 @@ public class BanqueroE {
         recursosMaximos.get(2).getRecursosNecesarios().add(4);
         recursosMaximos.get(3).getRecursosNecesarios().add(4);
         recursosMaximos.get(3).getRecursosNecesarios().add(2);
-        recursosMaximos.get(3).getRecursosNecesarios().add(2);
-        return recursosMaximos;
+        recursosMaximos.get(3).getRecursosNecesarios().add(2);*/
+ /*for (int i = 0; i < recursosMaximos.size(); i++) {
+            for (int j = 0; j < disponible.getDisponibles().size(); j++) {
+                recursosMaximos.get(i).getRecursosNecesarios().add(random.nextInt(disponible.getDisponibles().get(j)));
+            }
+        }*/
     }
 
-    public static Disponibles creaDisponibles() {
-        Disponibles lista = new Disponibles();
-        lista.getDisponibles().add(0);
-        lista.getDisponibles().add(1);
-        lista.getDisponibles().add(1);
+    public static void creaDisponibles() {
+        Scanner teclado = new Scanner(System.in);
+        //lista.getDisponibles().add(9);
+        //lista.getDisponibles().add(3);
+        //lista.getDisponibles().add(6);
+        //lista.getDisponibles().add(random.nextInt(7) + 3);
+        //lista.getDisponibles().add(random.nextInt(7) + 3);
+        //lista.getDisponibles().add(random.nextInt(7) + 3);
+        for (int i = 0; i < disponibles.getDisponibles().size(); i++) {
+            System.out.print("Introduzca el valor del Recurso " + i+": ");
+            int valor = teclado.nextInt();
+            disponibles.setDisponible(valor, i);
+        }
+    }
+
+    public static Disponibles actualizarDisponibles(Disponibles lista) {
+        lista.getDisponibles().set(0, 0);
+        lista.getDisponibles().set(1, 1);
+        lista.getDisponibles().set(2, 1);
         return lista;
     }
-
 }
